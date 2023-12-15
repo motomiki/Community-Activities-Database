94d9c3e6bd45b680b9669702147d62413948d820
KimitomoTadano <motomiki.lab@gmail.com>
2023-12-15 19:17:20 +0900

    database

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,12 +1,28 @@
+const fs = require('fs');
+
 module.exports = {
   HOST: process.env.MYSQL_HOST || "127.0.0.1",
   PORT: process.env.MYSQL_PORT || "3306",
   USERNAME: process.env.MYSQL_USERNAME || "admin",
   PASSWORD: process.env.MYSQL_PASSWORD || "admin5206",
-  // DATABASE: process.env.MYSQL_DATABASE || "tastylog",
   DATABASE: process.env.MYSQL_DATABASE || "localgroup",
   CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
     parseInt(process.env.MYSQL_CONNECTION_LIMIT) : 10,
   QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ?
-    parseInt(process.env.MYSQL_QUEUE_LIMIT) : 0
+    parseInt(process.env.MYSQL_QUEUE_LIMIT) : 0,
+  SSL: process.env.MYSQL_SSL_CA ?
+    { ca: fs.readFileSync(process.env.MYSQL_SSL_CA) } : null
 };
+
+
+// module.exports = {
+//   HOST: process.env.MYSQL_HOST || "127.0.0.1",
+//   PORT: process.env.MYSQL_PORT || "3306",
+//   USERNAME: process.env.MYSQL_USERNAME || "admin",
+//   PASSWORD: process.env.MYSQL_PASSWORD || "admin5206",
+//   DATABASE: process.env.MYSQL_DATABASE || "localgroup",
+//   CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
+//     parseInt(process.env.MYSQL_CONNECTION_LIMIT) : 10,
+//   QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ?
+//     parseInt(process.env.MYSQL_QUEUE_LIMIT) : 0
+// };
