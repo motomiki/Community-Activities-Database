c0fcccea0a234902d470b3cc4de8dcb7aaec22dc
KimitomoTadano <motomiki.lab@gmail.com>
2023-12-16 12:00:25 +0900

    update

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = {
   HOST: process.env.MYSQL_HOST || "127.0.0.1",
   PORT: process.env.MYSQL_PORT || "3306",
-  USERNAME: process.env.MYSQL_USERNAME || "admin",
+  USER: process.env.MYSQL_USERNAME || "admin",
   PASSWORD: process.env.MYSQL_PASSWORD || "admin5206",
   DATABASE: process.env.MYSQL_DATABASE || "localgroup",
   CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
