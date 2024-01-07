be4361133c5f303e0ea7899d86060b9baeb84f1a
KimitomoTadano <motomiki.lab@gmail.com>
2024-01-07 15:42:58 +0900

    update

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 module.exports = {
-  HOST: process.env.MYSQL_HOST || "127.0.0.1",
-  PORT: process.env.MYSQL_PORT || "3306",
-  USERNAME: process.env.MYSQL_USERNAME || "admin",
-  PASSWORD: process.env.MYSQL_PASSWORD || "admin5206",
-  DATABASE: process.env.MYSQL_DATABASE || "localgroup",
+  HOST: process.env.MYSQL_HOST,
+  PORT: process.env.MYSQL_PORT,
+  USERNAME: process.env.MYSQL_USERNAME,
+  PASSWORD: process.env.MYSQL_PASSWORD,
+  DATABASE: process.env.MYSQL_DATABASE,
   CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
     parseInt(process.env.MYSQL_CONNECTION_LIMIT) : 10,
   QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ?
@@ -14,16 +14,3 @@ module.exports = {
     ca: fs.readFileSync(process.env.MYSQL_SSL_CA || 'path_to_your_ssl_certificate')
   }
 };
-
-
-// module.exports = {
-//   HOST: process.env.MYSQL_HOST || "127.0.0.1",
-//   PORT: process.env.MYSQL_PORT || "3306",
-//   USERNAME: process.env.MYSQL_USERNAME || "admin",
-//   PASSWORD: process.env.MYSQL_PASSWORD || "admin5206",
-//   DATABASE: process.env.MYSQL_DATABASE || "localgroup",
-//   CONNECTION_LIMIT: process.env.MYSQL_CONNECTION_LIMIT ?
-//     parseInt(process.env.MYSQL_CONNECTION_LIMIT) : 10,
-//   QUEUE_LIMIT: process.env.MYSQL_QUEUE_LIMIT ?
-//     parseInt(process.env.MYSQL_QUEUE_LIMIT) : 0
-// };
